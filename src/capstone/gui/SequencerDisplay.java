547766261f443c3b885273131f11ebd4e71dd7a0
Michael King <mtking2@catamount.wcu.edu>
2015-09-22 13:49:56 -0400

    Fixed track setting using JSliders. Merged with new note grid.

--- src/capstone/gui/SequencerDisplay.java
+++ src/capstone/gui/SequencerDisplay.java
@@ -6,7 +6,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 /**
  * A graphical interface that represents a music sequencer.
@@ -16,7 +18,7 @@ import javax.swing.border.EtchedBorder;
  */
 public class SequencerDisplay extends JFrame implements ActionListener {
 
-    private JPanel panel, north, south, east, west, center;
+    private JPanel panel, north, south, east, west, center, track1, track2, track3, track4;
     private JMenuBar menuBar;
     private JMenu fileMenu;
     private JMenuItem newMenuItem, openMenuItem, exitMenuItem;
@@ -34,7 +36,7 @@ public class SequencerDisplay extends JFrame implements ActionListener {
         panel.setLayout(new BorderLayout());
         south = new JPanel();
         north = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        west = new JPanel(new GridLayout(5,1));
+        west = new JPanel(new GridLayout(4,1));
         east = new JPanel();
         south.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
         north.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
@@ -79,20 +81,9 @@ public class SequencerDisplay extends JFrame implements ActionListener {
         north.add(play);
         north.add(stop);
 
+        //west.add(new JSeparator());
 
-        west.add(new JSeparator());
-
-        JPanel track1Set = new JPanel();
-        track1Set.setLayout(new BoxLayout(track1Set, BoxLayout.Y_AXIS));
-        track1Set.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-        track1Set.add(new JSlider());
-        track1Set.add(new JButton("Btn"));
-        west.add(track1Set);
-
-
-        west.add(new JButton("Btn"));
-        west.add(new JButton("Btn"));
-        west.add(new JButton("Btn"));
+        trackSetup();
 
         
         panel.add(south, BorderLayout.SOUTH);
@@ -120,14 +111,138 @@ public class SequencerDisplay extends JFrame implements ActionListener {
         newMenuItem = new JMenuItem("New", KeyEvent.VK_N);
         fileMenu.add(newMenuItem);
 
-        // File->New, O - Mnemonic
-        openMenuItem = new JMenuItem("Open...", KeyEvent.VK_O);
-        fileMenu.add(openMenuItem);
-
         // File->New, X - Mnemonic
         exitMenuItem = new JMenuItem("Exit", KeyEvent.VK_X);
         exitMenuItem.addActionListener(this);
         fileMenu.add(exitMenuItem);
+    }
+
+    private void trackSetup() {
+        GridBagConstraints c = new GridBagConstraints();
+
+        track1 = new JPanel();
+        track1.setLayout(new GridBagLayout());
+
+        TitledBorder trackTitle = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED), "Track 1");
+        trackTitle.setTitleJustification(TitledBorder.LEFT);
+        track1.setBorder(trackTitle);
+
+        JSlider instSlider1 = new JSlider(0,127,0);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        c.gridy = 0;
+        track1.add(instSlider1,c);
+
+        JSlider pitchSlider1 = new JSlider(0,127,60);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(10,0,0,0);
+        c.gridx = 0;
+        c.gridy = 1;
+        track1.add(pitchSlider1,c);
+
+        JSlider volSlider1 = new JSlider(0,127,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 2;
+        track1.add(volSlider1,c);
+
+        west.add(track1);
+        //////////////////////////////////////////////////////////////////////
+
+        c = new GridBagConstraints();
+        track2 = new JPanel();
+        track2.setLayout(new GridBagLayout());
+
+        trackTitle = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED), "Track 2");
+        trackTitle.setTitleJustification(TitledBorder.LEFT);
+        track2.setBorder(trackTitle);
+
+        JSlider instSlider2 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        c.gridy = 0;
+        track2.add(instSlider2,c);
+
+        JSlider pitchSlider2 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(10,0,0,0);
+        c.gridx = 0;
+        c.gridy = 1;
+        track2.add(pitchSlider2,c);
+
+        JSlider volSlider2 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 2;
+        track2.add(volSlider2,c);
+
+        west.add(track2);
+        //////////////////////////////////////////////////////////////////////
+
+        c = new GridBagConstraints();
+        track3 = new JPanel();
+        track3.setLayout(new GridBagLayout());
+
+        trackTitle = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED), "Track 3");
+        trackTitle.setTitleJustification(TitledBorder.LEFT);
+        track3.setBorder(trackTitle);
+
+        JSlider instSlider3 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        c.gridy = 0;
+        track3.add(instSlider3,c);
+
+        JSlider pitchSlider3 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(10,0,0,0);
+        c.gridx = 0;
+        c.gridy = 1;
+        track3.add(pitchSlider3,c);
+
+        JSlider volSlider3 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 2;
+        track3.add(volSlider3,c);
+
+        west.add(track3);
+        //////////////////////////////////////////////////////////////////////
+
+        c = new GridBagConstraints();
+        track4 = new JPanel();
+        track4.setLayout(new GridBagLayout());
+
+        trackTitle = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED), "Track 4");
+        trackTitle.setTitleJustification(TitledBorder.LEFT);
+        track4.setBorder(trackTitle);
+
+        JSlider instSlider4 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        c.gridy = 0;
+        track4.add(instSlider4,c);
+
+        JSlider pitchSlider4 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(10,0,0,0);
+        c.gridx = 0;
+        c.gridy = 1;
+        track4.add(pitchSlider4,c);
+
+        JSlider volSlider4 = new JSlider(0,127);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 2;
+        track4.add(volSlider4,c);
+
+        west.add(track4);
+
+
     }
 
     @Override
