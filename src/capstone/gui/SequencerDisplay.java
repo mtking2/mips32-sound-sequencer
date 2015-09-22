5df0956e856133a43bc17b42165cbe2711f965d9
Michael King <mtking2@catamount.wcu.edu>
2015-09-22 12:24:08 -0400

    Begining to implement tracks and track settings.

--- src/capstone/gui/SequencerDisplay.java
+++ src/capstone/gui/SequencerDisplay.java
@@ -1,12 +1,12 @@
 package capstone.gui;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
 
 /**
  * A graphical interface that represents a music sequencer.
@@ -14,10 +14,13 @@ import javax.swing.JTextArea;
  * @author Brad Westley and Michael King
  * @version 9/17/15
  */
-public class SequencerDisplay extends JFrame {
+public class SequencerDisplay extends JFrame implements ActionListener {
 
     private JPanel panel, north, south, east, west;
     private JTable center;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem newMenuItem, openMenuItem, saveAsMenuItem, exitMenuItem;
     private JButton play, stop;
 
     /**
@@ -30,24 +33,28 @@ public class SequencerDisplay extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle(title);
         this.setResizable(false);
+        menuInit();
 
         panel = new JPanel();
-        
         panel.setLayout(new BorderLayout());
-
         south = new JPanel();
-        north = new JPanel();
-        west = new JPanel();
+        north = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        west = new JPanel(new GridLayout(5,1));
         east = new JPanel();
-        
-        
+        south.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        north.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        east.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        west.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+
+
         // 4 tracks, 16 beats
         
         int tracks = 4;
         int beats = 16;
         
         center = new NoteTable(tracks, beats, south);
-        
+        center.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+
         for(int i = 0; i < tracks; i++){
         	for(int j = 0; j < beats; j++){
         		center.setValueAt(i + "/" + j, i, j);
@@ -55,10 +62,28 @@ public class SequencerDisplay extends JFrame {
         }
 
         play = new JButton("Play");
+        play.setContentAreaFilled(false);
         stop = new JButton("Stop");
+        stop.setContentAreaFilled(false);
 
         north.add(play);
         north.add(stop);
+
+
+        west.add(new JSeparator());
+
+        JPanel track1Set = new JPanel();
+        track1Set.setLayout(new BoxLayout(track1Set, BoxLayout.Y_AXIS));
+        track1Set.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        track1Set.add(new JSlider());
+        track1Set.add(new JButton("Btn"));
+        west.add(track1Set);
+
+
+        west.add(new JButton("Btn"));
+        west.add(new JButton("Btn"));
+        west.add(new JButton("Btn"));
+
         
         panel.add(south, BorderLayout.SOUTH);
         panel.add(north, BorderLayout.NORTH);
@@ -67,8 +92,38 @@ public class SequencerDisplay extends JFrame {
         panel.add(west, BorderLayout.WEST);
 
         this.setContentPane(panel);
+        this.setJMenuBar(menuBar);
         this.setSize(width, height);
         this.setVisible(true);
 
+    }
+
+    private void menuInit() {
+        menuBar = new JMenuBar();
+
+        // File Menu, F - Mnemonic
+        fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(fileMenu);
+
+        // File->New, N - Mnemonic
+        newMenuItem = new JMenuItem("New", KeyEvent.VK_N);
+        fileMenu.add(newMenuItem);
+
+        // File->New, O - Mnemonic
+        openMenuItem = new JMenuItem("Open...", KeyEvent.VK_O);
+        fileMenu.add(openMenuItem);
+
+        // File->New, X - Mnemonic
+        exitMenuItem = new JMenuItem("Exit", KeyEvent.VK_X);
+        exitMenuItem.addActionListener(this);
+        fileMenu.add(exitMenuItem);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(exitMenuItem)) {
+            System.exit(0);
+        }
     }
 }
