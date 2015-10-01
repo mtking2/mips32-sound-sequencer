0d564096b685f27039fece38495e4fbe4f0b237b
Brad Westley <bawestley1@gmail.com>
2015-10-01 14:01:16 -0400

    Revamped code to handle four instrument tracks at the same time. Unstable build, has bugs at runtime.

--- src/capstone/mips/MipsSequencer.asm
+++ src/capstone/mips/MipsSequencer.asm
@@ -1,92 +1,158 @@
 .data
 
-instruments:	.word 1, 19, 34, 42
+instruments:	.word 1, 1, 1, 1
 
-# TRACK 1 #
-pitchArray1:		 .word  64,  66, 67,  69,  71,   73,  74,  76	# The eight pitches for the 8 notes (-1 = rest)
-durationArray1:	 .word 200  200, 200, 200, 200, 200, 200, 200 		# Keep duration for each note
-volumeArray1:	 .word 127, 127, 127, 127, 127, 127, 127, 127	# Volume for each note
+# TRACK 1 DATA #
 
-# TRACK 2 #
+pitchArray1:	.word  64,  66,  67,  69,  71,  73,  74,  76	# The pitch for each note
+durationArray1:	.word 200  200, 200, 200, 200, 200, 200, 200 	# The duration of each note
+volumeArray1:	.word 127, 127, 127, 127, 127, 127, 127, 127	# The volume of each note
 
-pitchArray2:		 .word  64,  62, 60,  59,  57,   55,  54,  52	# The eight pitches for the 8 notes (-1 = rest)
-durationArray2:	 .word 200  200, 200, 200, 200, 200, 200, 200 		# Keep duration for each note
-volumeArray2:	 .word 127, 127, 127, 127, 127, 127, 127, 127	# Volume for each note
+# TRACK 2 DATA #
+
+pitchArray2:	.word  67,  69,  71,  73,  74,  76,  78,  79	# The pitch for each note
+durationArray2: .word 200  200, 200, 200, 200, 200, 200, 200 	# The duration of each note
+volumeArray2:	.word 127, 127, 127, 127, 127, 127, 127, 127	# The volume of each note
+
+# TRACK 3 DATA #
+
+pitchArray3:	.word  71,  73,  74,  76,  78,  79,  81,  83	# The pitch for each note
+durationArray3:	.word 200  200, 200, 200, 200, 200, 200, 200 	# The duration of each note
+volumeArray3:	.word 127, 127, 127, 127, 127, 127, 127, 127	# The volume of each note
+
+# TRACK 4 DATA #
+
+pitchArray4:	.word  76,  78,  79,  81,  83,  85,  86,  88	# The pitch for each note
+durationArray4: .word 200  200, 200, 200, 200, 200, 200, 200 	# The duration of each note
+volumeArray4:	.word 127, 127, 127, 127, 127, 127, 127, 127	# The volume of each note
 
 
 .text
 
 
 setup:
-	la $t0, pitchArray1	# Use $t0 to store current pitch address
-	la $t1, durationArray1	# Use $t1 to store current duration
-	la $t2, volumeArray1	# Use $t2 to store volume
-
-	la $t7, pitchArray2	# Use $t7 to store current pitch address		# HORRIBLE CODE CHANGE LATER
-	la $t8, durationArray2	# Use $t8 to store current duration
-	la $t9, volumeArray2	# Use $t9 to store volume
-	
-	li $t3, 0	# Used to count how many times loop has finished
+	li $t3, 0	# How many times loop has finished
 	li $t4, 8	# How many times the loop will run
-	li $t5, 0      	# How many notes have been played
+	li $t5, 0      	# Which note is being played
 	li $t6, 8	# How many notes to play
-	
+
+	li $t7, 400	# Load tempo
+	li $t8, 0	# Offset for array values
+
 main:
-	jal playSequence
+	jal loadFirstTrack
+	jal playNote
+	jal loadSecondTrack
+	jal playNote
+	jal loadThirdTrack
+	jal playNote
+	jal loadFourthTrack
+	jal playNote
+	jal sleep
 
-playSequence:
-	li $v0, 37 	# Load service
-	li $a2, 0   # Load instrument
-	
-	lw $a0, 0($t0)		# Load current pitch into param
-	addi $t0, $t0, 4	# Increment pitch address by 4
+	addi $t5, $t5, 1	# Increment notes played by 1
 
-	li $a1, 300
+	beq $t5, $t6, reset
+	j main
+
+
+playNote:
+
+	li $v0, 37 	# Load MIDI playing service
 	
-	lw $a3, 0($t2)		# Load current volume into param
-	addi $t2, $t2, 4	# Increment volume address by 4
+	lw $a0, 0($t0)		# Load current pitch
+
+	lw $a1, 0($t1)		# Load current duration
+	
+	lw $a3, 0($t2)		# Load current volume
 
 	syscall		# Play sound
-
-	li $a2, 22   # Load instrument
 	
-	lw $a0, 0($t7)		# Load current pitch into param
-	addi $t7, $t7, 4	# Increment pitch address by 4
+	addi $t8, $t8, 4	# Increment offset by 1 byte
 
-	li $a1, 300
+	jr $ra		# Jump to return address
 	
-	lw $a3, 0($t9)		# Load current volume into param
-	addi $t9, $t9, 4	# Increment volume address by 4
-
-	syscall		# Play sound
-
-	noteSleep:
-		li $v0, 32
-		lw $a0, 0($t8)
-		addi $t8, $t8, 4	# Increment duration address by 4
-		syscall
-
-	addi $t5, $t5, 1	# Increment times looped by 1
-	
-	bne $t5, $t6, main	# If amount of notes to play is not reached, play next note
-
 reset:
-	
-	la $t0, pitchArray1	# Use $t0 to store current pitch address
-	la $t1, durationArray1	# Use $t1 to store current duration
-	la $t2, volumeArray1	# Use $t2 to store volume
 
-	la $t7, pitchArray2	# Use $t7 to store current pitch address
-	la $t8, durationArray2	# Use $t8 to store current duration
-	la $t9, volumeArray2	# Use $t9 to store volume
-
-Loop:
 	addi $t3, $t3, 1	# Increment times looped by 1
-	li $t5, 0   # How many notes have been played
+	
+	li $t5, 0  	# How many notes have been played
+	li $t8, 0	# Reset offset to 0
+
+	# Reset to first track data
+	jal loadFirstTrack
+
 	beq $t3, $t4, exit
 	j main
+
+sleep:
+	# Sleep until next beat
+	li $v0, 32		# Load sleep service
+	add $a0, $0, $t7	# Time to sleep == tempo
+	syscall
+
+	jr $ra
+
+loadFirstTrack:
+	la $t0, pitchArray1	# $t0 = first track's pitch data
+	la $t1, durationArray1	# $t1 = first track's duration data
+	la $t2, volumeArray1	# $t3 = first track's volume data
+
+	# Add offset to array addresses
+	add $t0, $t0, $t8
+	add $t1, $t1, $t8
+	add $t2, $t2, $t8
+
+	la $a2, instruments	# Load ADDRESS of instrument array into param
+	lw $a2, 0($a2)		# Load second instrument from address (offset for instrument 1 == 0 bytes)
+
+	jr $ra			# Jump to return address
+
+loadSecondTrack:
+	la $t0, pitchArray2	# $t0 = second track's pitch data
+	la $t1, durationArray2	# $t1 = second track's duration data
+	la $t2, volumeArray2	# $t3 = second track's volume data
+
+	# Add offset to array addresses
+	add $t0, $t0, $t8
+	add $t1, $t1, $t8
+	add $t2, $t2, $t8
+
+	la $a2, instruments	# Load ADDRESS of instrument array into param
+	lw $a2, 4($a2)		# Load second instrument from address (offset for instrument 1 == 4 bytes)
+
+	jr $ra			# Jump to return address
+
+loadThirdTrack:
+	la $t0, pitchArray3	# $t0 = third track's pitch data
+	la $t1, durationArray3	# $t1 = third track's duration data
+	la $t2, volumeArray3	# $t3 = third track's volume data
+
+	# Add offset to array addresses
+	add $t0, $t0, $t8
+	add $t1, $t1, $t8
+	add $t2, $t2, $t8
+
+	la $a2, instruments	# Load ADDRESS of instrument array into param
+	lw $a2, 8($a2)		# Load third instrument from address (offset for instrument 1 == 8 bytes)
+
+	jr $ra			# Jump to return address
+
+loadFourthTrack:
+	la $t0, pitchArray4	# $t0 = fourth track's pitch data
+	la $t1, durationArray4	# $t1 = fourth track's duration data
+	la $t2, volumeArray4	# $t3 = fourth track's volume data
+
+	# Add offset to array addresses
+	add $t0, $t0, $t8
+	add $t1, $t1, $t8
+	add $t2, $t2, $t8
+
+	la $a2, instruments	# Load ADDRESS of instrument array into param
+	lw $a2, 12($a2)		# Load fourth instrument from address (offset for instrument 1 == 12 bytes)
+
+	jr $ra			# Jump to return address
 
 exit:
 	li $v0, 10	# Load exit system call
 	syscall
-	
